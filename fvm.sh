d956c347b195a2a91f6428ae0d7efa177d48c556
xinfeng-tech <616782041@qq.com>
2019-09-29 14:48:09 +0800

    update:color full

--- fvm.sh
+++ fvm.sh
@@ -11,8 +11,22 @@ case "`uname`" in
     Darwin*) darwin=true;;
 esac
 
+function print_blue(){
+  echo -e "\033[36m$1\033[0m"
+}
+function print_green(){
+  echo -e "\033[32m$1\033[0m"
+}
+function print_yellow(){
+  echo -e "\033[33m$1\033[0m"
+}
+function print_red(){
+  echo -e "\033[31m$1\033[0m"
+}
+
+
 function print_help(){
-    echo "Flutter SDK versions Manager."
+    print_blue "Flutter SDK versions Manager."
     echo ""
     echo "Usage: fvm <command> [arguments]"
     echo ""
@@ -27,9 +41,9 @@ function print_help(){
 }
 
 function list(){
-    echo "current => `current`"
+    print_green "current => `current`"
     echo ""
-    echo "installed versions:"
+    print_blue "installed versions:"
     for version in `ls -1 "${THIS_DIR}/versions"`
     do
       echo "${version} => `cat ${THIS_DIR}/versions/${version}/version`"
@@ -43,7 +57,7 @@ function current(){
 }
 
 function print_current_version(){
-    echo "Now using flutter => version:`current`"
+    print_blue "Now using flutter => version:`current`"
     flutter --version
 }
 
@@ -53,15 +67,15 @@ function use(){
     if [[ -z ${version} ]];then
       version='default'
     fi
-    echo "Switch flutter to => version:${version}"
+    print_blue "Switch flutter to => version:${version}"
     local current_dir=$THIS_DIR/current
     local target_version_dir=$THIS_DIR/versions/$version
     if [[ ! -d ${target_version_dir} ]];then
-      echo "Error: version:${version} has not installed!!"
+      print_red "Error: version:${version} has not installed!!"
       exit 1
     fi
     if [[ ! -f ${target_version_dir}/bin/flutter ]];then
-      echo "Error: version:${version} is not a valid flutter-sdk !!"
+      print_red "Error: version:${version} is not a valid flutter-sdk !!"
       exit 2
     fi
     rm -rf $current_dir
@@ -80,7 +94,7 @@ function install(){
   local version_zip=""
   version_zip=`list_remote | grep "$version_key" | awk 'NR==1'`
   if [[ -z ${version_zip}  ]];then
-    echo "Error: no flutter version match $version_key !!"
+    print_red "Error: no flutter version match $version_key !!"
     exit 1
   fi
   local version_short=`echo $version_zip | awk -F '_v' '{print $2}' | awk -F '.zip' '{print $1}'`
@@ -88,15 +102,15 @@ function install(){
   local temp_zip="${TMPDIR}fvm/flutter.zip"
   local target_dir="${THIS_DIR}/versions/${version_short}"
   if [[ -d ${target_dir} ]];then
-    echo "Error: flutter $version_short seems to has installed ,please check it!!"
+    print_red "Error: flutter $version_short seems to has installed ,please check it!!"
     exit 1
   fi
   rm -rf $temp_zip
   mkdir -p `dirname $temp_zip`
-  echo "flutter $version_short is downloading..."
+  print_green "flutter $version_short is downloading..."
   curl --progress-bar -o $temp_zip $download_url
   unzip -o $temp_zip -d $target_dir
-  echo "flutter $version_short has installed to $target_dir!"
+  print_blue "flutter $version_short has installed to $target_dir!"
   rm -rf $temp_zip
 }
 
