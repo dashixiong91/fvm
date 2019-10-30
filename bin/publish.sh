171f08a0439e85553f122cd2c34026069a60ac19
qianxinfeng <qianxinfeng@meituan.com>
2019-10-31 00:38:34 +0800

    ci: push all tag before publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,6 +9,7 @@ function main(){
   local pkg_file="${THIS_DIR}/../package.json"
   local pkg_version=`cat ${pkg_file} | grep "\"version\"" | awk 'NR==1' | awk -F '\"' '{print $4}'`
   echo -e "\033[32m publish version:${pkg_version} to homebrew... \033[0m"
+  git push --tag
   sh "${THIS_DIR}/update_formula.sh" $pkg_version
   echo -e "\033[36m publish version:${pkg_version} success! \033[0m"
 }
