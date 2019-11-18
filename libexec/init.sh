bb88b76fa9d9d2e60ad76bae39e663ea30c1a184
qianxinfeng <qianxinfeng@meituan.com>
2019-11-18 14:43:37 +0800

    fix: init error when blank in $PATH

--- libexec/init.sh
+++ libexec/init.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-for element in `fvm init`
-do
-  export $element
-done
+function main(){
+  local OLD_IFS="$IFS"
+  IFS=$'\n'
+  for element in `fvm init`
+  do
+    export $element
+  done
+  IFS="$OLD_IFS"
+}
+
+main "$@"
